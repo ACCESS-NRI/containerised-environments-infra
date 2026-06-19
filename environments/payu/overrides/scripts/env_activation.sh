bdd5d48b11dc181c36d1609904148f6f315bbe17
Davide Marchegiani <davide.marchegiani@anu.edu.au>
2026-06-19 10:26:18 +1000

    Fixed env activation script with correct path for conda activation scripts (#66)

--- environments/payu/overrides/scripts/env_activation.sh
+++ environments/payu/overrides/scripts/env_activation.sh
@@ -16,7 +16,7 @@ export CONDA_PROMPT_MODIFIER="__ENV_PROMPT_MODIFIER__"
 # Payu specific environment variables
 export ENV_LAUNCHER_SCRIPT_PATH="__LAUNCHER_SCRIPT_PATH__"
 
-for file in "$env_path"/conda/activate.d/*.sh; do
+for file in "$env_path"/etc/conda/activate.d/*.sh; do
     if [ -r "$file" ]; then
         . "$file"
     fi
